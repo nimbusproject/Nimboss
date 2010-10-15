c2722dbef25aee825747f81e100ff2eb09219ebb
David LaBissoniere <labisso@gmail.com>
2010-10-15 10:15:28 -0700

    Adjusted nimboss example

--- examples/examples.py
+++ examples/examples.py
@@ -1,8 +1,10 @@
 import os
 from nimboss.broker import BrokerClient
 from nimboss.cluster import NimbusClusterDriver
+from nimboss.node import NimbusNodeDriver
 
 BROKER_URI = "http://mybroker.edu"
+NIMBUS_URI = "http://mycloud.edu"
 
 
 def main():
@@ -11,7 +13,8 @@ def main():
     # in this example we are using the same credentials for broker and nimbus
     # this would be different if we were launching on say, EC2
     brokerclient = BrokerClient(BROKER_URI, key, secret)
-    cdriver = NimbusClusterDriver(key, secret, brokerclient)
+    ndriver = NimbusNodeDriver(key, secret, host=NIMBUS_URI) 
+    cdriver = NimbusClusterDriver(brokerclient, node_driver=ndriver)
 
     clusterdoc = open("/path/to/myclusterdoc.xml").read()
     cluster = cdriver.create_cluster(clusterdoc) # return value is a Cluster
