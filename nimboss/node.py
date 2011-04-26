464c8410c3d4d54502fd2084ea27eea03f1342ad
David LaBissoniere <labisso@uchicago.edu>
2011-04-26 12:52:39 -0500

    Made xpath adjustment optional in NimbusNodeDriver
    Nimbus XML is more EC2-like in Nimbus 2.7+

--- nimboss/node.py
+++ nimboss/node.py
@@ -11,8 +11,16 @@ class NimbusNodeDriver(EC2NodeDriver):
     connectionCls = NimbusConnection
     name = "Nimbus"
 
+    def __init__(self, *args, **kwargs):
+        self.fix_xpath = not kwargs.pop('ex_oldnimbus_xml', False)
+        EC2NodeDriver.__init__(self, *args, **kwargs)
+
     def _fixxpath(self, xpath):
-        # Nimbus return types don't include namespace declaration in every
+        # old (pre 2.7) Nimbus return types don't include namespace declaration in every
         # tag, unlike EC2. So we override this method to make xpath queries
         # just pass through instead of being ns-prefixed.
-        return xpath
+
+        if self.fix_xpath:
+            return EC2NodeDriver._fixxpath(self, xpath)
+        else:
+            return xpath
